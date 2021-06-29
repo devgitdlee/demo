b64426f8bee5b18ee97acf1e87ce36e84726cc1f
devgitdlee <devgitdlee@gmail.com>
2021-06-29 17:50:05 +0900

    user 객체 생성 및 board에 userid 연관관계 생성

---
+++ src/main/java/com/example/o2project/demo/Controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.example.o2project.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @RequestMapping(value = "/api/test", method = RequestMethod.GET)
+    // @respone
+    public String getApiTest() {
+        return "";
+    }
+}
