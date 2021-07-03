cbaa15b1d35874858ba12e42f50e194b48b891f3
devgitdlee <devgitdlee@gmail.com>
2021-07-03 14:13:54 +0900

    dao db 연동 수행중

--- src/main/java/com/example/o2project/demo/Controller/DummyController.java
+++ src/main/java/com/example/o2project/demo/Controller/DummyController.java
@@ -7,6 +7,9 @@ import com.example.o2project.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DummyController {
@@ -19,7 +22,20 @@ public class DummyController {
         System.out.println(user.getId());
         user.setRole(RoleType.USER);
         userRepository.save(user);
+
         return "회원가입 완료했습니다.";
 
     }
+
+    @GetMapping(value="/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+        User user = userRepository.findById(id).orElseGet(supplier<>()){
+            @override
+            public get(){
+                return new 
+            };
+        }
+        return  user;
+    }
+
 }
