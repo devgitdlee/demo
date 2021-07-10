3bb7ae611eda9c9129e16e7932a415ebe56f61fc
devgitdlee <devgitdlee@gmail.com>
2021-07-10 16:05:04 +0900

    jackson 및 전체 조회 및 스프링부트 내장 페이징 처리

--- src/main/java/com/example/o2project/demo/Controller/DummyController.java
+++ src/main/java/com/example/o2project/demo/Controller/DummyController.java
@@ -1,5 +1,6 @@
 package com.example.o2project.demo.Controller;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.example.o2project.demo.Repository.UserRepository;
@@ -7,11 +8,14 @@ import com.example.o2project.demo.model.RoleType;
 import com.example.o2project.demo.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DummyController {
@@ -45,7 +49,27 @@ public class DummyController {
                 return new IllegalArgumentException("해당 유저는 없는 유저입니다." + id);
             }
         });
+        /*
+         * 요청: 웹브라우저 user 객체를 -> JSON 변환 스프링부트는 MessageConverter라는 애가 응답시에 자동 작동 만약에 자바
+         * 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서 user 오브젝트를 JSON으로 변환해서
+         * 브라우저에게 던져주는 메소드
+         */
         return user;
+    }
+
+    @GetMapping("/dummy/user")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * 스프링부트 자동 페이지 처리 메소드 한페이지당 2건에 데이터를 리턴받는 형식
+     */
+    @GetMapping("/dummy/user/page")
+    public Page<User> pageList(
+            @PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
+        return users;
     }
 
 }
